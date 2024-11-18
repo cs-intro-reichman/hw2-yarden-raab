d8b47c768a0334556561ea2be2ff502a402875c7
yarden raab <yarden.raab@gmail.com>
2024-11-18 13:53:38 +0200

    Time Calculation 1

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,34 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        
+        
+
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+		// Does the same with the minutes part of the input.
+		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        // Replace this comment with the rest of your code
+        int minutesAdd = Integer.parseInt(args[1]);
+        
+        int totalMinuts = hours*60 + minutes + minutesAdd;
+        int newHour = (totalMinuts / 60 ) % 24;
+        int newMinutes = totalMinuts % 60;
+        
+        if (newHour>9 && newMinutes>9) {
+            System.out.println( newHour + ":" + newMinutes );
+        }
+        else {
+            if (newHour<10 && newMinutes<10) {
+                System.out.println( "0" +newHour + ":0" + newMinutes );  
+            }
+            else {
+                if (newHour<10){
+                    System.out.println( "0" +newHour + ":" + newMinutes ); 
+                }
+                else{
+                    System.out.println(newHour + ":0" + newMinutes ); 
+                }
+            }
+        }
+        
     }
 }
