22d617b715b7932a7e3fdcd93ec4caf93b75a431
yarden raab <yarden.raab@gmail.com>
2024-11-18 18:51:03 +0200

    Collatz Conjecture 1

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,58 @@
 // Demonstrates the Collatz conjecture.
+
+import javax.print.DocFlavor.STRING;
+
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    	
+		int n = Integer.parseInt(args[0]);
+		String mode = args[1];
+		int seed =1;
+		int count = 1;
+
+		if (mode.equals("c")) {
+			for (int i=1; i<=n; i++){
+				seed=i;
+				count=1;
+				if (seed == 1){
+					System.out.println("1 4 2 1 (4)");
+				}
+				else {
+					while (seed != 1) {
+						System.out.print(seed + " ");
+						if (seed % 2 ==0) {
+							seed = seed / 2;
+						}
+						else {
+							seed = seed * 3 + 1;
+						}
+						count++;		
+					}
+					System.out.print(seed + " ");
+					System.out.print("(" + count + ")");
+					System.out.println();
+				}			
+			}
+			System.out.println("Every one of the first " + n + " hailstone sequences reached 1.");
+		}
+		else {
+			for (int i=1; i<=n; i++) {
+				while (seed != 1) {
+					System.out.print(seed + " ");
+					if (seed % 2 ==0) {
+						seed = seed / 2;
+					}
+					else {
+						seed = seed * 3 + 1;
+					}
+				}
+			}
+			
+			System.out.println("Every one of the first " + n + " hailstone sequences reached 1.");
+		}
+
+		
+	
 	}
+	
 }
