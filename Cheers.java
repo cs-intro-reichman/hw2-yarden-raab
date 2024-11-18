3cd02c3cea8982ac219870c1eb205232edbc7920
yarden raab <yarden.raab@gmail.com>
2024-11-18 15:33:35 +0200

    Cheers 5

--- Cheers.java
+++ Cheers.java
@@ -9,10 +9,10 @@ public class Cheers {
           for (int i=0; i<s.length(); i++)    {
                temp= s.charAt(i);
                 if (an.indexOf(temp) !=-1) {
-                        System.out.println("Give me an " + temp + ": " + temp);
+                        System.out.println("Give me an " + temp + ": " + temp + "!");
                 }
                 else {
-                        System.out.println("Give me a  " + temp + ": " + temp); 
+                        System.out.println("Give me a  " + temp + ": " + temp + "!"); 
                 }   
         }      
           System.out.println("What does that spell?");
